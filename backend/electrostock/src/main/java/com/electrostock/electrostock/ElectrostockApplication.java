57eb6b7c9e5eb07dbc4479594a182ef3c27314f4
unknown <fatihabouzar450@gmail.com>
2025-11-05 19:22:06 +0100

    Initial Spring Boot REST API project with WebSocket config

---
+++ backend/electrostock/src/main/java/com/electrostock/electrostock/ElectrostockApplication.java
@@ -0,0 +1,13 @@
+package com.electrostock.electrostock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectrostockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectrostockApplication.class, args);
+    }
+
+}
---
+++ backend/electrostock/src/test/java/com/electrostock/electrostock/ElectrostockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electrostock.electrostock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectrostockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
