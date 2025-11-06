4619664749553963c040cf10dab173110db225ed
unknown <fatihabouzar450@gmail.com>
2025-11-06 21:42:55 +0100

    adding category and product entities

---
+++ backend/electrostock/src/main/java/com/electrostock/backend/entity/Category.java
@@ -0,0 +1,21 @@
+package com.electrostock.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+}
