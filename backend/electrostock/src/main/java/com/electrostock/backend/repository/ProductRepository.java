0082c5a9094a3972feede5aa6e99f3984a08b55e
unknown <fatihabouzar450@gmail.com>
2025-11-13 13:25:18 +0100

    adding Product repository

---
+++ backend/electrostock/src/main/java/com/electrostock/backend/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.electrostock.backend.repository;
+
+
+import com.electrostock.backend.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
+
