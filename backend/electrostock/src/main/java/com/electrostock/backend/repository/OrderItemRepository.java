6ec95161385a4d7f35a9b53344a964df2c25e7ca
unknown <fatihabouzar450@gmail.com>
2025-11-16 11:06:25 +0100

    adding order item repository

---
+++ backend/electrostock/src/main/java/com/electrostock/backend/repository/OrderItemRepository.java
@@ -0,0 +1,13 @@
+package com.electrostock.backend.repository;
+
+
+import com.electrostock.backend.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+
+}
