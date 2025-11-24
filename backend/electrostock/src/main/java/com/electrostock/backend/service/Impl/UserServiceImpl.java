a293be37d452583184bfd27ce74af1b200565e59
unknown <fatihabouzar450@gmail.com>
2025-11-24 18:50:37 +0100

    adding implementation of services

--- backend/electrostock/src/main/java/com/electrostock/backend/service/Impl/UserServiceImpl.java
+++ backend/electrostock/src/main/java/com/electrostock/backend/service/Impl/UserServiceImpl.java
@@ -1,4 +1,38 @@
 package com.electrostock.backend.service.Impl;
 
-public class UserServiceImpl {
+
+import com.electrostock.backend.entity.User;
+import com.electrostock.backend.repository.UserRepository;
+import com.electrostock.backend.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User saveUser(User user){
+        return userRepository.save(user);
+
+    }
+
+    @Override
+    public User findByUsername(String username){
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+    }
+
+    @Override
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+
+    }
 }
