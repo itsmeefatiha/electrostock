08d2ee3ac57294f3be48fb094f751baa5e98ed89
unknown <fatihabouzar450@gmail.com>
2025-11-27 21:08:15 +0100

    adding some modifications to product controller

--- backend/electrostock/src/main/java/com/electrostock/backend/controller/ProductController.java
+++ backend/electrostock/src/main/java/com/electrostock/backend/controller/ProductController.java
@@ -1,4 +1,31 @@
 package com.electrostock.backend.controller;
 
+
+import com.electrostock.backend.entity.Product;
+import com.electrostock.backend.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@CrossOrigin
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+}
+    @GetMapping
+    public List<Product> getAllProducts()
+    {
+        return productService.getAllProducts();
+    }
+
+
 }
