c0f57f57597ed5605ee608d30289dbbb83265ec6
unknown <fatihabouzar450@gmail.com>
2025-11-28 22:37:28 +0100

    adding order and user controller

---
+++ backend/electrostock/src/main/java/com/electrostock/backend/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.electrostock.backend.controller;
+
+import com.electrostock.backend.entity.User;
+import com.electrostock.backend.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{username}")
+    public User getUser(@PathVariable String username) {
+        return userService.findByUsername(username);
+    }
+}
+
